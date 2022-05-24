c0ed9d173710e54b350c8125daa13d49a9943ad6
Chen Gao <chen0gao@gmail.com>
2022-05-23 20:14:09 -0400

    User Profile Page #11 Draft

---
+++ src/User/ClassBadge.js
@@ -0,0 +1,16 @@
+import Chip from "@mui/material/Chip";
+import "../assets/css/ClassBadge.css";
+
+export default function ClassBadge({ course }) {
+  return (
+    <Chip
+      className="courseChip"
+      label={
+        <div>
+          <div>{course.split(" ")[0]}</div>
+          <div>{course.split(" ")[1]}</div>
+        </div>
+      }
+    />
+  );
+}
